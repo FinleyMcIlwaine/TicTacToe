681b7646e74683543f450fcae8b5f6992f0df21b
Finley McIlwaine <finleymcilwaine@gmail.com>
2018-11-06 09:10:57 -0700

    App fully functional. Need to abstract.

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,46 @@
+## TicTacToe
+## By Finley McIlwaine
+
 from tkinter import *
 
 # global turn variable
 # 1 -> X's turn
 # 0 -> O's turn
 turn = 1
+# global allow move variable
+# 1 -> not allowed
+# 0 -> allowed
+allowMove = 0
+# global # of turns variable
+# -> tells number of turns
+#    made during round
+numTurns = 0
+
+def clearState():
+    global turn
+    global board
+    global allowMove
+    global numTurns
+    # reset the turn label
+    boardLabel.config(text="X's turn!", font=("Bookman", 15), bg="#808080", fg="white")
+    # reset the turn variable
+    turn = 1
+    # reset the allow move bool
+    allowMove = 0
+    # reset the number of turns for the round
+    numTurns = 0
+
+    # reinitialize the board
+    board = [
+        boardSpot([0,0]),boardSpot([0,1]),boardSpot([0,2]),
+        boardSpot([1,0]),boardSpot([1,1]),boardSpot([1,2]),
+        boardSpot([2,0]),boardSpot([2,1]),boardSpot([2,2]),
+    ]
 
 def checkState(board):
+    global allowMove
+    global numTurns
+    # board states for X winning
     test=1
     Xwinner = (
         (board[0].value==test and board[1].value==test and board[2].value==test) or
@@ -16,6 +51,8 @@ def checkState(board):
         (board[2].value==test and board[5].value==test and board[8].value==test) or
         (board[0].value==test and board[4].value==test and board[8].value==test) or
         (board[2].value==test and board[4].value==test and board[6].value==test))
+
+    # board states for O winning
     test=0
     Owinner = (
         (board[0].value==test and board[1].value==test and board[2].value==test) or
@@ -26,13 +63,25 @@ def checkState(board):
         (board[2].value==test and board[5].value==test and board[8].value==test) or
         (board[0].value==test and board[4].value==test and board[8].value==test) or
         (board[2].value==test and board[4].value==test and board[6].value==test))
+
+    # board state for a tie
+    tie = (numTurns==9 and not (Owinner or Xwinner))
+
+    # check states
+    if tie:
+        boardLabel.config(text="It's a tie!", bg="white", fg="red")
+        allowMove = 1
     if Xwinner:
-        boardLabel.config(text="X wins!!", bg="white", fg="red")
+        boardLabel.config(text="X wins!!", bg="white", fg="green")
+        allowMove = 1
     if Owinner:
-        boardLabel.config(text="O wins!!", bg="white", fg="red")
+        boardLabel.config(text="O wins!!", bg="white", fg="green")
+        allowMove = 1
+    
 
 class boardSpot:
     def __init__(self, ind):
+        # initialize board spot members
         self.value = -1
         self.ind = ind
         self.btn = Button(boardFrame, bg="#333333", image=empty, command=self.clicked)
@@ -40,7 +89,11 @@ class boardSpot:
 
     # handles move input
     def clicked(self):
-        if self.value!=-1:
+        global allowMove
+
+        # check if the spot already has a value
+        # and if moves are allowed
+        if self.value!=-1 or allowMove==1:
             return None
         else:
             self.makeMove()
@@ -48,7 +101,12 @@ class boardSpot:
     # exectutes a move
     def makeMove(self):
         global turn
+        global numTurns
+
+        # set the value of the board spot
         self.value = turn
+        # check whose turn it was and set
+        # board state appropriately
         if turn==1:
             self.btn.config(image=xImg)
             turn = 0
@@ -57,11 +115,16 @@ class boardSpot:
             self.btn.config(image=oImg)
             turn = 1
             boardLabel.config(text="X's turn!")
+        
+        # increment number of turns
+        numTurns += 1
         # check for win state
         checkState(board)
 
 # set up window
 root = Tk()
+root.title("TicTacToe!")
+root.iconbitmap("./pngs/favicon.ico")
 root.geometry('500x500')
 root.resizable(0, 0)
 root.configure(background='#808080')
@@ -80,12 +143,18 @@ board = [
     boardSpot([2,0]),boardSpot([2,1]),boardSpot([2,2]),
 ]
 
-# const header label
-headLabel = Label(root, text="TicTacToe!", font=("Impact",20))
-headLabel.pack(side='top', pady=(10,0))
+# const header labels
+titleLabel = Label(root, text="TicTacToe!", font=("Impact",20))
+titleLabel.pack(fill=X)
+nameLabel = Label(root, text="By Finley McIlwaine", font=("Bookman",8))
+nameLabel.pack(fill=X)
+
+# reset button
+resetBtn = Button(root, bg="#808080", fg="white", text="Reset", font=("Bookman",10), height=1, width=15, command=clearState)
+resetBtn.pack(pady=(5,0))
 
 # label object to tell turn/state
 boardLabel = Label(root, text="X's turn!", font=("Bookman", 15), bg="#808080", fg="white")
-boardLabel.pack(side='top', pady=(50,0))
+boardLabel.pack(pady=(30,0))
 
 root.mainloop()
